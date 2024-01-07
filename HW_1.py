7e14f345fad2b086eaba6430cf5e13b5e8a2855e
Tatiana Andreeva <tanysha20072@gmail.com>
2024-01-07 16:08:15 +0300

    1_HW_correct

--- HW_1.py
+++ HW_1.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/base/')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/index/')
 def index():
     return render_template('index.html')
 
-@app.route('/clothing')
+
+@app.route('/clothing/')
 def clothing():
     return render_template('clothing.html')
 
-@app.route('/shoes')
+
+@app.route('/shoes/')
 def shoes():
     return render_template('shoes.html')
 
-@app.route('/jackets')
-def jacket():
+@app.route('/jackets/')
+def jackets():
     return render_template('jackets.html')
 
 if __name__ == '__main__':
